a71ad9547ca410960067ae5d73dff5082b2cd866
paulbricman <paulbricman@protonmail.com>
2021-10-03 11:12:29 +0200

    Fix adaptation, similarity, alignment + validate against tests

--- main.py
+++ main.py
@@ -1,31 +1,10 @@
-from conceptorflow import Conceptor, alignment, similarity
-import conceptorflow as cf
-import numpy as np
 
-state_cloud = np.random.rand(3, 3)
-C1 = Conceptor().from_states(state_cloud, 0.1)
-print(C1.conceptor_matrix)
 
-state_cloud = np.random.rand(3, 3)
-C2 = Conceptor().from_states(state_cloud, 0.1)
-print(C2.conceptor_matrix)
+states1 = np.random.rand(3, 3)
+c1 = Conceptor().from_states(states1, 0.1)
+print(c1.conceptor_matrix)
 
-C3 = cf.conjunction([C1, C2])
-C4 = cf.disjunction([C1, C2])
+states2 = np.random.rand(3, 3)
+c2 = Conceptor().from_states(states2, 0.1)
+print(c2.conceptor_matrix)
 
-print(cf.compare(C1, C3), cf.compare(C1, C4))
-
-C5 = cf.aperture_adaptation(cf.aperture_adaptation(C1, 0.2), 0.1)
-print(C1.conceptor_matrix)
-print(C5.conceptor_matrix)
-
-print('SVD')
-print(C1.conceptor_matrix)
-
-print('similarity')
-print(similarity(C1, C1))
-print(similarity(C1, C4))
-
-print('alignment')
-print(alignment(C2, state_cloud[0]))
-print(alignment(C3, state_cloud[0]))
