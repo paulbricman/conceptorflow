99638ab59e1690f12d7f4664a87ceb7307d61249
Paul Bricman <paulbricman@protonmail.com>
2021-12-07 11:03:31 +0100

    feat: implement simulated annealing for finding trees

--- diff.py
+++ diff.py
@@ -2,6 +2,8 @@ from conceptors import *
 import pickle
 import numpy as np
 import os
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 
 def diff(c1, c2):    
@@ -45,6 +47,68 @@ def compute_adjacency_matrix():
     return [list(concepts.keys()), diffs]
 
 
-diffs = compute_adjacency_matrix()
-print(diffs)
-pickle.dump(diffs, open('diffs/diffs.pickle', 'wb+'))
+def eval_solution(mask, adjacency_matrix, heuristic='mean'):
+    prunes = 0
+    vals = 0
+    treeness = 0
+    
+    for row_idx, row in enumerate(mask):
+        for col_idx, col in enumerate(row):
+            if row_idx != col_idx:
+                if col == True:
+                    prunes += 1
+                else:
+                    if heuristic == 'mean':
+                        vals += adjacency_matrix[row_idx][col_idx][0]
+                        #print('vals', vals)
+                    elif heuristic == 'median':
+                        vals += adjacency_matrix[row_idx][col_idx][1]
+                    elif heuristic == 'pos_ratio':
+                        vals += adjacency_matrix[row_idx][col_idx][2] - 0.5
+                    elif heuristic == 'area_ratio':
+                        vals += adjacency_matrix[row_idx][col_idx][3] - 0.5
+
+    for col_idx, col in enumerate(np.transpose(mask)):
+        treeness -= np.abs(len([row for row in col if row is False]) - 1)
+
+    return prunes * 0.01 + vals + treeness * 0.01
+
+
+def simulated_annealing(epochs=10000, heuristic='mean'):
+    weights = pickle.load(open('diffs/diffs.pickle', 'rb'))[1]
+    state = np.zeros((len(weights), len(weights)), dtype=bool)
+    evals = []
+
+    for epoch in tqdm(range(epochs)):
+        temperature = (epochs - epoch) / epochs
+        if temperature == 0:
+            return state
+        
+        next = state.copy()
+        next_row = np.random.randint(0, len(weights) - 1)
+        next_col = np.random.randint(0, len(weights) - 1)
+        next[next_row, next_col] = not next[next_row, next_col]
+
+        next_eval = eval_solution(next, weights, heuristic)
+        current_eval = eval_solution(state, weights, heuristic)
+        evals += [current_eval]
+        
+        delta = next_eval - current_eval
+        if delta > 0:
+            state = next
+        else:
+            if np.random.uniform(0, 1) < (np.e ** (delta / temperature)):
+                state = next
+
+    plt.plot(evals)
+    plt.show()
+
+    return state
+
+
+mask = simulated_annealing()
+pickle.dump(mask, open('diffs/mask.pickle', 'wb+'))
+
+# diffs = compute_adjacency_matrix()
+# print(diffs)
+# pickle.dump(diffs, open('diffs/diffs.pickle', 'wb+'))
